7e5e23d867edd067ef5e24e7cb0cd8f4901ee842
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2025-11-29 07:22:45 +0100

    Update classes.py

--- fcc/classes.py
+++ fcc/classes.py
@@ -82,3 +82,66 @@ cart.remove('Ergo keyboard')
 print(cart.list_items()) # ['Laptop', 'Wireless mouse', 'Monitor']
 
 cart.remove('banana') # banana is not in cart
+
+# handling object attributes dynamically
+print(getattr(reyna, 'color')) # checking if an attribute exists
+print(getattr(reyna, 'colouur', 'Attribute does not exist')) # Attribute does not exist shows if the attribute is not found
+
+# using dir() to see all attributes and methods of an object
+for stuff in dir(book1):
+    if not stuff.startswith('__') and not callable(getattr(book1, stuff)): #ignoring dunder methods and other methods
+        print(f'{stuff} is an attribute of book1')
+
+# using setattr() to set or modify an attribute
+class Team:
+    pass
+
+team1 = Team()
+setattr(team1, 'type', 'Football') # sets an attribute 'type' with value 'Football' for the team1 object
+
+print(team1.type)
+wolves_team = {
+    'name': 'Wolves of Steel',
+    'city': 'Detroit',
+    'players': 22,
+    'titles': 1,
+    'describe_team': lambda self: print(f'{self.name} from {self.city} has {self.players} players and has won {self.titles} titles as a {self.type} team.')
+}
+
+for attr_name, attr_val in wolves_team.items():
+    setattr(team1, attr_name, attr_val)
+
+team1.describe_team(team1)
+# using hasattr() to check if an attribute exists
+print(hasattr(team1, 'Country')) # False
+print(hasattr(team1, 'type')) # True
+
+required_attrs = ['name', 'city', 'country', 'players', 'titles']
+for attr in required_attrs:
+    if not hasattr(team1, attr):
+        print(f"ERROR: Team is missing the required attribute: '{attr}'.")
+    else:
+        print(f"{attr}: {getattr(team1, attr)}")
+
+# deleting an attribute using delattr()
+# delattr(team1, 'country') will raise an error since 'country attribute does not exist 
+# so we need to check first using hasattr()
+class Bike:
+    def __init__(self, brand, model, year, buyer, seller, price):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.buyer = buyer
+        self.seller = seller
+        self.price = price
+bike1 = Bike('Yamaha', 'FZ-07', 2020, 'Alice', 'Bob', 1700)
+attr_to_remove = ['buyer', 'seller', 'price', 'place', 'warranty', 'color']
+for attr in attr_to_remove:
+    if hasattr(bike1, attr):
+        delattr(bike1, attr)
+        print(f"Removed attrubute: '{attr}.' ")
+print('\nFinal attributes remaining:')
+for attr in dir(bike1):
+    if not attr.startswith("__") and not callable(getattr(bike1, attr)):
+        print(f'{attr}: {getattr(bike1, attr)}')
+    
