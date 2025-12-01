503d871abf86f131b5e7d06502a8ec1980534fcb
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2025-12-01 09:39:26 +0100

    Add spending chart functionality to budget.py
    Implement create_spend_chart function to display spending categories.

--- fcc/budget.py
+++ fcc/budget.py
@@ -55,15 +55,28 @@ class Category:
         return f"{f_line}\n{trs_line}{total_trans}"
 
 def create_spend_chart(categories):
-    pass
+    print('Percentage spent by category')
+    for c in categories:
+        for tr in c.ledger:
+            if not tr['description'].startswith('Transfer') and tr['amount'] < 1:
+                print(type(tr['amount']), tr['amount'])
 
 cat1 = Category('purse')
 cat2 = Category('wallet')
+cat3 = Category('drinks')
+cat4 = Category('clothing')
 cat1.deposit(2000, 'from Mum')
-cat2.deposit(1000, 'new deposit')
+cat2.deposit(1900, 'new deposit')
+cat3.deposit(8000, 'First deposit')
+cat4.deposit(7000, 'First deposit')
 # print(cat1.ledger)
 cat1.transfer(500, cat2)
 cat2.withdraw(200.88, 'zobo purchase')
-print(cat1.ledger)
-print(cat2.ledger)
-print(cat2)
+cat1.withdraw(250.25, 'crypto')
+cat3.withdraw(700.55, 'Energy drink')
+cat4.withdraw(950.08, 'Shirt')
+# print(cat1.ledger)
+# print(cat2.ledger)
+# print(cat4)
+
+create_spend_chart([cat1,cat2,cat3,cat4])
